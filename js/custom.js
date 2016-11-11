19d6e21132011f1a04289223045df20ea18678a8
simplesphere <anoop.jeewoolall@gmail.com>
2016-11-11 15:41:11 -0500

    typer + mods

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,18 @@
 jQuery(function($){'use strict';(function(){$('#preloader').delay(200).fadeOut('slow');}());$('.left-col-block, .right-col-block').theiaStickySidebar();});
+
+$(function(){
+
+  $("#typed").typed({
+    stringsElement: $('#typed-strings'),
+    typeSpeed: 100,
+    backDelay: 3000,
+    loop: true,
+    contentType: 'html',
+    loopCount: false,
+    callback: function(){
+
+    },
+    resetCallback: function() { newTyped(); }
+  });
+
+});
